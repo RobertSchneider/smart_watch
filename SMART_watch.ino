bb0361a9622bf1362ff67b860de487587e7972ff
Robert Schneider <rs@vibrantcore.com>
2015-06-07 22:40:06 +0200

    working with app

--- SMART_watch.ino
+++ SMART_watch.ino
@@ -1,4 +1,3 @@
- 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -13,7 +12,6 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
-
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16 
 
@@ -21,63 +19,31 @@ Adafruit_SSD1306 display(OLED_RESET);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-
-// Connect CLK/MISO/MOSI to hardware SPI
-// e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
-#define ADAFRUITBLE_REQ 10
-#define ADAFRUITBLE_RDY 1     // This should be an interrupt pin, on Uno thats #2 or #3
-#define ADAFRUITBLE_RST 9
+#define ADAFRUITBLE_REQ 9
+#define ADAFRUITBLE_RDY 1
+#define ADAFRUITBLE_RST 10
 
 Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 
 void setup()   {                
   Serial.begin(9600);
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
   Serial.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
-
-  // BTLEserial.setDeviceName("NEWNAME"); /* 7 characters max! */
-
+  BTLEserial.setDeviceName("WATCH");
   BTLEserial.begin();
-  
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
+  display.setRotation(0);
   display.display();
-  delay(2000);
-
-  // Clear the buffer.
+  delay(200);
   display.clearDisplay();
-
-  // draw a single pixel
-  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
   display.display();
-  delay(2000);
-  display.clearDisplay();
-  
-  // draw the first ~12 characters in the font
-  //testdrawchar();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
 }
 
 aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
 void loop() {
-  // Tell the nRF8001 to do whatever it should be working on.
+  
   BTLEserial.pollACI();
-
-  // Ask what is our current status
   aci_evt_opcode_t status = BTLEserial.getState();
-  // If the status changed....
   if (status != laststatus) {
-    // print it out!
     if (status == ACI_EVT_DEVICE_STARTED) {
         Serial.println(F("* Advertising started"));
     }
@@ -87,56 +53,38 @@ void loop() {
     if (status == ACI_EVT_DISCONNECTED) {
         Serial.println(F("* Disconnected or advertising timed out"));
     }
-    // OK set the last status change to this one
     laststatus = status;
   }
 
   if (status == ACI_EVT_CONNECTED) {
-    // Lets see if there's any data for us!
     if (BTLEserial.available()) {
       Serial.print("* "); Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
     }
-    // OK while we still have something to read, get a character and print it out
+    char c[4]; int cnt = 0;
     while (BTLEserial.available()) {
-      char c = BTLEserial.read();
-      Serial.print(c);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-      display.write(c);
-  display.display();
+      char c2 = BTLEserial.read();
+      if(cnt <= 3) {
+        c[cnt] = c2;
+      }
+      cnt++;
     }
-
-    // Next up, see if we have any data to get from the Serial console
-
+    if(cnt == 4) {
+      char x = c[0];
+      char y = c[1];
+      char d = c[2];
+      char d2 = c[3];
+      display.setData((int16_t)x, (int16_t)y, (int16_t)(d<<8)+d2);
+    }
+    display.display();
+    
     if (Serial.available()) {
-      // Read a line from Serial
       Serial.setTimeout(100); // 100 millisecond timeout
       String s = Serial.readString();
-
-      // We need to convert the line to bytes, no more than 20 at this time
       uint8_t sendbuffer[20];
       s.getBytes(sendbuffer, 20);
       char sendbuffersize = min(20, s.length());
-
       Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
-
-      // write the data
       BTLEserial.write(sendbuffer, sendbuffersize);
     }
   }
-}
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
 }
